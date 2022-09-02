457ec16bcaebbc7f595e6a20a42612b06ab4f952
Felipe Santos <felipe.dossantos@agrisolus.com.br>
2022-09-02 15:16:44 -0300

    criando banco de dados e conectando ao synapse

--- main.tf
+++ main.tf
@@ -21,10 +21,6 @@ variable "resource_group_name" {
   default = "synapse-learn"
 }
 
-output "resource_group_id" {
-  value = azurerm_resource_group.synapse_rg.id
-}
-
 resource "azurerm_resource_group" "synapse_rg" {
   name     = var.resource_group_name
   location = "brazilsouth"
@@ -34,12 +30,17 @@ data "external" "myipaddr" {
   program = ["bash", "-c", "curl -s 'https://api.ipify.org?format=json'"]
 }
 
+data "azurerm_client_config" "current" {
+}
+
 output "my_public_ip" {
   value = data.external.myipaddr.result.ip
 }
 
-data "azurerm_client_config" "current" {
+output "resource_group_id" {
+  value = azurerm_resource_group.synapse_rg.id
 }
+
 
 resource "azurerm_storage_account" "synapse_blob" {
   name                     = "synapselearnblob"
@@ -82,21 +83,6 @@ resource "azurerm_synapse_firewall_rule" "synapse_firewall" {
   ]
 }
 
-resource "azurerm_synapse_linked_service" "blob_linked_service" {
-  name                 = "synapse_learn_blob_linked_service"
-  synapse_workspace_id = azurerm_synapse_workspace.synapse_workspace.id
-  type                 = "AzureBlobStorage"
-  type_properties_json = <<JSON
-{
-  "connectionString": "${azurerm_storage_account.synapse_blob.primary_connection_string}"
-}
-JSON
-
-  depends_on = [
-    azurerm_synapse_firewall_rule.synapse_firewall,
-  ]
-}
-
 resource "azurerm_role_assignment" "synapse_role" {
   scope                = azurerm_storage_account.synapse_blob.id
   role_definition_name = "Storage Blob Data Contributor"
@@ -108,4 +94,57 @@ resource "azurerm_synapse_sql_pool" "default_pool" {
   synapse_workspace_id = azurerm_synapse_workspace.synapse_workspace.id
   sku_name             = "DW100c"
   create_mode          = "Default"
+}
+
+
+resource "azurerm_postgresql_flexible_server" "synapse_pg" {
+  name                   = "synapse-pg"
+  resource_group_name    = azurerm_resource_group.synapse_rg.name
+  location               = azurerm_resource_group.synapse_rg.location
+  version                = "14"
+  administrator_login    = "psqladmin"
+  administrator_password = "H@Sh1CoR3!"
+  zone                   = "1"
+
+  storage_mb = 32768
+  sku_name   = "B_Standard_B1ms"
+}
+
+resource "azurerm_postgresql_flexible_server_firewall_rule" "synapse_pg" {
+  name             = "me"
+  server_id        = azurerm_postgresql_flexible_server.synapse_pg.id
+  start_ip_address = "0.0.0.0"         # data.external.myipaddr.result.ip
+  end_ip_address   = "255.255.255.255" #data.external.myipaddr.result.ip
+}
+
+
+resource "azurerm_postgresql_flexible_server_database" "synapse_db" {
+  name      = "synapse-db"
+  server_id = azurerm_postgresql_flexible_server.synapse_pg.id
+  collation = "en_US.utf8"
+  charset   = "utf8"
+}
+
+# resource "azurerm_synapse_managed_private_endpoint" "synapse_pvt_pg" {
+#   name                 = "synapse-pg-pvt"
+#   synapse_workspace_id = azurerm_synapse_workspace.synapse_workspace.id
+#   target_resource_id   = azurerm_postgresql_flexible_server.synapse_pg.id
+#   subresource_name     = "postgresqlServer"
+
+#   depends_on = [azurerm_postgresql_flexible_server_firewall_rule.synapse_pg]
+# }
+
+resource "azurerm_synapse_linked_service" "pg_linked_service" {
+  name                 = "synapse_learn_pg_linked_service"
+  synapse_workspace_id = azurerm_synapse_workspace.synapse_workspace.id
+  type                 = "AzurePostgreSql"
+  type_properties_json = <<JSON
+{
+  "connectionString": "Server=${azurerm_postgresql_flexible_server.synapse_pg.fqdn};Database=synapse-db;Port=5432;UID=${azurerm_postgresql_flexible_server.synapse_pg.administrator_login};Password=${azurerm_postgresql_flexible_server.synapse_pg.administrator_password};Ssl Mode=Require;"
+}
+JSON
+
+  depends_on = [
+    azurerm_postgresql_flexible_server_firewall_rule.synapse_pg
+  ]
 }
